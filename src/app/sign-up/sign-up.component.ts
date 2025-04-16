ff0acf56572072492bcdb6eb60b68ee3e070ccb9
oussamasine07 <sineoussama@gmail.com>
2025-04-16 12:00:42 +0100

    save new user to localstorage

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { PageTitleComponent } from '../page-title/page-title.component';
+import { SignUpService } from '../services/signup/sign-up.service';
+import { FormsModule, NgForm } from "@angular/forms";
 
 @Component({
   selector: 'app-sign-up',
-  imports: [ PageTitleComponent ],
+  imports: [ PageTitleComponent, FormsModule ],
   templateUrl: './sign-up.component.html',
-  styleUrl: './sign-up.component.css'
+  styleUrl: './sign-up.component.css',
+  standalone: true
 })
 export class SignUpComponent {
-  title = "Welcome to QUIZIZZ"
+  title = signal("Welcome to QUIZIZZ")
+
+  userObj = {
+    username: ""
+  }
+
+  userInfo = {
+    username: "",
+    quizzes: []
+  }
+
+  onSubmit ( form: NgForm ) {
+    const { username } = this.userObj;
+
+    this.userInfo = {
+      ...this.userInfo,
+      username
+    }
+
+    localStorage.setItem("user", JSON.stringify(this.userInfo));
+
+    this.userObj = {
+      username: ""
+    }
+    
+  }
+
+  
+
 }
